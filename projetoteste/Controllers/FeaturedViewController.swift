f22714bcdd04c893f86e73fbb01cfaeda3eeb6ff
Daniel Monterazo <dmm2@academy.cin.ufpe.br>
2022-07-11 17:09:50 -0300

    Adiciona outlets e começa a implementação da popular collection view

---
+++ projetoteste/Controllers/FeaturedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FeaturedViewController.swift
+//  projetoteste
+//
+//  Created by dmm on 04/07/22.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController, UICollectionViewDataSource {
+    
+    
+    @IBOutlet var popularCollectionView: UICollectionView!
+    @IBOutlet var nowPlayingCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        popularCollectionView.dataSource = self
+    }
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell {
+        
+        cell.titleLabel.text = "Título do Filme"
+        cell.image.image = UIImage()
+        
+        return cell
+    }
+ return UICollectionViewCell()
+}}
+
---
+++ projetoteste/View/NowPlayingCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NowPlayingCollectionViewCell.swift
+//  projetoteste
+//
+//  Created by dmm on 11/07/22.
+//
+
+import UIKit
+
+class NowPlayingCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
